37cddbc02482c106bef28a933321db295842b5ae
Martha <mleccles@outlook.com>
2020-12-28 21:16:33 +0000

    adds index.js files into data folders

---
+++ db/data/development-data/index.js
@@ -0,0 +1,6 @@
+const topicData = require("./topics.js");
+const userData = require("./users.js");
+const commentData = require("./comments.js");
+const articleData = require("./articles");
+
+module.exports = { topicData, userData, commentData, articleData };
---
+++ db/data/test-data/index.js
@@ -0,0 +1,11 @@
+const topicData = require("/topics");
+const userData = require("/users");
+const commentData = require("/comments");
+const articleData = require("/articles");
+
+module.exports = {
+  articleData,
+  commentData,
+  topicData,
+  userData,
+};
