edb89c94707925f0eadaa0d2a4da732d632d8e83
Martha <mleccles@outlook.com>
2020-12-29 20:47:48 +0000

    sets up app test file

---
+++ __tests__/app.test.js
@@ -0,0 +1,13 @@
+process.env.NODE_ENV = "test";
+const app = require("../app");
+const request = require("supertest");
+const connection = require("../db/connection");
+
+describe("/api", () => {
+  beforeEach(() => {
+    return connection.seed.run();
+  });
+  afterAll(() => {
+    return connection.destroy();
+  });
+});
