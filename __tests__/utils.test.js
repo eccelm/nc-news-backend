fbfa67824a69855646046005c4acf53f9a39fe7f
Martha <mleccles@outlook.com>
2020-12-29 20:17:59 +0000

    tests the commentFormat utility function

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,9 +1,10 @@
 const {
   formatDate,
   createReferenceObj,
+  formatComments,
 } = require("../db/utils/data-manipulation");
 
-describe("formateDate - Format Date function", () => {
+describe("formateDate", () => {
   test("should return a new array", () => {
     const testArray = [{ key: "hello" }];
     const returnValue = formatDate(testArray);
@@ -75,7 +76,7 @@ describe("formateDate - Format Date function", () => {
   });
 });
 
-describe("createReferenceObj - Create Reference Object function", () => {
+describe("createReferenceObj", () => {
   test("should return an empty object when passed an empty array", () => {
     const emptyArray = [];
     const expectedOutput = {};
@@ -148,5 +149,149 @@ describe("createReferenceObj - Create Reference Object function", () => {
         Z: 7,
       }
     );
+  });
+});
+
+describe.only("formatComments", () => {
+  test("should return a new array", () => {
+    const singleComment = [
+      {
+        body:
+          "The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.",
+        belongs_to: "Living in the shadow of a great man",
+        created_by: "butter_bridge",
+        votes: 14,
+        created_at: 1479818163389,
+      },
+    ];
+    const referenceObj = {
+      "Living in the shadow of a great man": 1,
+    };
+    expect(formatComments(singleComment, referenceObj)).toEqual(
+      expect.any(Array)
+    );
+    expect(formatComments(singleComment, referenceObj)).not.toEqual([
+      {
+        body:
+          "The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.",
+        belongs_to: "Living in the shadow of a great man",
+        created_by: "butter_bridge",
+        votes: 14,
+        created_at: 1479818163389,
+      },
+    ]);
+  });
+  test("the new array should contain a new object with the correctly formatted keys, when passed a single comment", () => {
+    const singleComment = [
+      {
+        body:
+          "The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.",
+        belongs_to: "Living in the shadow of a great man",
+        created_by: "butter_bridge",
+        votes: 14,
+        created_at: 1479818163389,
+      },
+    ];
+    const referenceObj = {
+      "Living in the shadow of a great man": 1,
+    };
+    const expectedOutput = [
+      {
+        body:
+          "The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.",
+        author: "butter_bridge",
+        article_id: 1,
+        votes: 14,
+        created_at: 1479818163389,
+      },
+    ];
+    expect(formatComments(singleComment, referenceObj)).toEqual(expectedOutput);
+  });
+  test("should return an array of comment objects in the correct format when passed an array with multiple comments", () => {
+    const inputComments = [
+      {
+        body:
+          "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        belongs_to: "They're not exactly dogs, are they?",
+        created_by: "butter_bridge",
+        votes: 16,
+        created_at: 1511354163389,
+      },
+      {
+        body:
+          "The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.",
+        belongs_to: "Living in the shadow of a great man",
+        created_by: "butter_bridge",
+        votes: 14,
+        created_at: 1479818163389,
+      },
+    ];
+    const refObject = {
+      "Living in the shadow of a great man": 1,
+      "They're not exactly dogs, are they?": 9,
+    };
+
+    const expectedOutput = [
+      {
+        body:
+          "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        article_id: 9,
+        author: "butter_bridge",
+        votes: 16,
+        created_at: 1511354163389,
+      },
+      {
+        body:
+          "The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.",
+        author: "butter_bridge",
+        article_id: 1,
+        votes: 14,
+        created_at: 1479818163389,
+      },
+    ];
+    expect(formatComments(inputComments, refObject)).toEqual(expectedOutput);
+  });
+
+  test("should  not mutate the input comments array", () => {
+    const input = [
+      {
+        body:
+          "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        belongs_to: "They're not exactly dogs, are they?",
+        created_by: "butter_bridge",
+        votes: 16,
+        created_at: 1511354163389,
+      },
+      {
+        body:
+          "The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.",
+        belongs_to: "Living in the shadow of a great man",
+        created_by: "butter_bridge",
+        votes: 14,
+        created_at: 1479818163389,
+      },
+    ];
+    const refObject = {
+      "Living in the shadow of a great man": 1,
+    };
+    formatComments(input, refObject);
+    expect(input).toEqual([
+      {
+        body:
+          "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        belongs_to: "They're not exactly dogs, are they?",
+        created_by: "butter_bridge",
+        votes: 16,
+        created_at: 1511354163389,
+      },
+      {
+        body:
+          "The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.",
+        belongs_to: "Living in the shadow of a great man",
+        created_by: "butter_bridge",
+        votes: 14,
+        created_at: 1479818163389,
+      },
+    ]);
   });
 });
--- db/utils/data-manipulation.js
+++ db/utils/data-manipulation.js
@@ -17,4 +17,15 @@ const createReferenceObj = (rows, nameKey, idKey) => {
   return ref;
 };
 
-module.exports = { formatDate, createReferenceObj };
+const formatComments = (commentData, articleRef) => {
+  return commentData.map(({ belongs_to, created_by, ...restOfComment }) => {
+    const newComment = {
+      article_id: articleRef[belongs_to],
+      author: created_by,
+      ...restOfComment,
+    };
+    return newComment;
+  });
+};
+
+module.exports = { formatDate, createReferenceObj, formatComments };
