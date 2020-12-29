f02da640993755d245d4a0a2fee7e07f42dba1cb
Martha <mleccles@outlook.com>
2020-12-29 18:03:51 +0000

    implements formatDate in seed file for articles

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -5,6 +5,22 @@ const {
   userData,
 } = require("../data/index.js");
 
+const { formatDate } = require("../utils/data-manipulation");
+
 exports.seed = function (knex) {
-  // add seeding functionality here
+  return knex.migrate
+    .rollback()
+    .then(() => {
+      return knex.migrate.latest();
+    })
+    .then(() => {
+      return knex.insert(topicData).into("topics").returning("*");
+    })
+    .then(() => {
+      return knex.insert(userData).into("users").returning("*");
+    })
+    .then(() => {
+      const formatArticle = formatDate(articleData);
+      return knex.insert(formatArticle).into("articles").returning("*");
+    });
 };
