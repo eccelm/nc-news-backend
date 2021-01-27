0fe7f2aacf7ebfc6051943f42cd6f78c2650b063
Martha <mleccles@outlook.com>
2021-01-27 13:07:14 +0000

    reinstates accidentally deleted topics router

---
+++ routers/topics.js
@@ -0,0 +1,5 @@
+const topicsRouter = require("express").Router();
+const { getAllTopics } = require("../controllers/topics");
+
+topicsRouter.route("/").get(getAllTopics);
+module.exports = topicsRouter;
